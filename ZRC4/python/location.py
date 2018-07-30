7778ff8a8c78164d767551a5f014c833f1e3e059
lukeperlowski <lukeperlowski@gmail.com>
2018-07-30 09:30:20 -0400

    Added ZRC4 directory and added new files location and track items

---
+++ ZRC4/python/location.py
@@ -0,0 +1,13 @@
+class Location_coordinates:
+	#this class holds the locations in coordinates of every relevant place for this challenge
+	break_room = [0,0]
+	task_list = [0,3]
+	S11 = [2,7]
+	S12 = [1,7]
+	S13 = [0,7]
+	A11 = [7,6]
+	A12 = [6,6]
+	A13 = [5,6]
+	A21 = [7,3]
+	A22 = [6,3]
+	A23 = [5,3]
---
+++ ZRC4/python/trackitems.py
@@ -0,0 +1,43 @@
+class Items:
+	def __init__(self):
+		self.itemID = [0]*7
+		self.current_location = [0]*7
+		self.destination = [0]*7
+	
+	def populate_items(self, ID_array, shelf_array, spot_array):
+		for i in range(0, 7):
+			self.destination[i] = shelf_array[i] + spot_array[i]
+			self.itemID[i] = ID_array[i]
+			self.current_location[i] = 0
+
+	def update_location(self, location, ID):
+	#this function is to update the current location of after interacting with it
+		for i in range(0,(len(self.itemID) - 1)):
+			if self.itemID[i] == ID:
+				index = i
+				break
+		self.current_location[index] = location	
+
+	def get_location(self, ID):
+	#this function is used to get the current location of a block 
+		for i in range(0,(len(self.itemID) - 1)):
+			if self.itemID[i] == ID:
+				index = i
+				break
+		return self.current_location[index]
+
+	def match_destination(self, location):
+	#this function is used to see if a location read in matches a destination of a block, and if so will return that blocks ID
+		for i in range(0,(len(self.itemID) - 1)):
+			if self.destination[i] == location:
+				return self.itemID[i]
+		return 0
+
+	def all_done(self):
+	#this function checks to see if all blocks current locations match their destinations
+	#if one current location is not in sync with its corresponding destinaiton, then the algorithm isn't done and we return 0
+	#if we make it through the for loop without returning a 0 that means all of the currents match the destinations and we are all done, we return 1
+		for i in range(0,(len(self.itemID) - 1)):
+			if self.current_location[i] != self.destination[i]: 			
+				return 0
+		return 1
